577d5fecc2292a2bbf5c6ce416b7f73c11219742
PStackebrandt <PStackebrandt@PSTACKEBRAW10>
2017-08-16 16:07:06 +0200

    Created data provider

--- CodingChallenge/CheckForEight.cs
+++ CodingChallenge/CheckForEight.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodingChallenge
 {
     public class CheckForEight
     {
+        private readonly DataProvider _dataProvider;
+
         public CheckForEight()
         {
             Console.WriteLine($"{nameof(CheckForEight)} wird erstellt");
+            _dataProvider = new DataProvider();
+        }
+
+        public DataProvider DataProvider
+        {
+            get { return _dataProvider; }
         }
 
         public void Run()
         {
             Console.WriteLine($"{nameof(CheckForEight)}.{nameof(CheckForEight.Run)}()");
+
+            var data = new DataProvider.GetData(first);
         }
     }
 }
---
+++ CodingChallenge/DataProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace CodingChallenge
+{
+    public class DataProvider
+    {
+        /// <summary>
+        /// Identifies a data set.
+        /// </summary>
+        internal const string FirstDataSet = "first";
+
+        /// <summary>
+        /// Identifies a data set.
+        /// </summary>
+        internal const string SecondDataSet = "second";
+
+
+        public DataProvider()
+        {
+        }
+
+        private IEnumerable<int> GetDataSet(string which)
+        {
+            var data = new List<int>();
+
+            switch (which)
+            {
+                case nameof(DataProvider.FirstDataSet):
+                    data.AddRange(new[] { 1, 2, 3, 9 });
+                    break;
+
+                case nameof(DataProvider.SecondDataSet):
+                    data.AddRange(new[] { 1, 2, 4, 4 });
+                    break;
+            }
+            
+            return data;
+        }
+    }
+}
